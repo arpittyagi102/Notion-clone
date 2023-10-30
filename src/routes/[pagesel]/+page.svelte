e55cf7737a0d0228d71f02a6a11a0a92700141dc
Arpit Tyagi <arpittyagi102@gmail.com>
2023-10-30 14:19:28 +0530

    trying to edit content

--- src/routes/[pagesel]/+page.svelte
+++ src/routes/[pagesel]/+page.svelte
@@ -1,29 +1,38 @@
 <script>
-    import { page } from "$app/stores";
     import { pagesData } from "../store";
     import { pageSelected } from "../store";
 
-    const pagesel = $page.params.pagesel;
     $: allData = $pagesData;
     $: selectedData = allData.find(findData => findData.id == $pageSelected);
 
     function handleTitleChange(e){
+        console.log(e.target);
         selectedData.title = e.target.value;
         $pagesData = $pagesData
-        debug();
     }
 
-    function debug(){
-        console.log("all data is " , allData);
-        console.log("pagesData is " , $pagesData);
+    function handleContentChange(e){
+        selectedData.content = e.target.innerText;
     }
 
 </script>
 
-<div class="flex justify-center items-center grow">
-    <input 
-        class=" bg-transparent text-5xl focus:outline-none" 
-        value={selectedData.title}
-        on:blur={handleTitleChange}
+<div class="flex flex-col grow p-32 px-48">
+    <strong>
+        <input 
+            class=" bg-transparent text-4xl focus:outline-none w-full" 
+            value={selectedData.title}
+            on:blur={handleTitleChange}
+        />
+    </strong>
+    <div
+        class="text-lg pt-5 focus: outline-none"
+        contenteditable
+        on:blur={handleContentChange}
     >
+        {selectedData.content}
+    </div>
+    <button on:click={()=>{console.log(selectedData)}}>
+        Debug
+    </button>
 </div>
